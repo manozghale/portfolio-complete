d31ef1bc54a2644f7fa8e21425a912175c88f3cc
manozghale <manozghale@gmail.com>
2024-12-19 13:43:01 +0545

    update codes for production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: colors.blue,
+                secondary: colors.slate,
+                accent: colors.lime,
+            },
+        },
+    },
+    plugins: [],
+}
